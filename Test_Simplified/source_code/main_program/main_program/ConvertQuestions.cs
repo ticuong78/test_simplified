cfa453710a1f6aa164d781dcb25c07fbaad92472
ticuong78 <132206463+ticuong78@users.noreply.github.com>
2024-03-19 22:36:00 +0700

    Add files via upload

---
+++ Test_Simplified/source_code/main_program/main_program/ConvertQuestions.cs
@@ -0,0 +1,54 @@
+﻿using DocumentFormat.OpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace main_program
+{
+    class ConvertQuestions
+    {
+        public List<Question> ConvertedQuestions { get; set; } = new();
+
+        public ConvertQuestions(IEnumerable<OpenXmlElement> rawOpenXmlElementList) 
+        {
+            Question question = new Question();
+            bool shouldAdd = false;
+            foreach (var rawElement in rawOpenXmlElementList)
+            {
+                string innerText = rawElement.InnerText;
+
+                //Loại bỏ các phần tử không thuộc Tệp Trắc Nghiệm như: tiêu đề, mức độ, ...
+                if (innerText.StartsWith("Câu 1")) shouldAdd = true;
+                if (!shouldAdd || innerText == " ") continue;
+                //-------------------------------------------------------------------------
+
+                if (innerText.StartsWith("Câu "))
+                {
+                    question = new();
+                    question.AddMainQuestion(innerText.Trim());
+                }
+                else
+                {
+                    innerText = Regex.Replace(innerText, "[A-Z]\\.", "");
+                    question.AddOptions(innerText.Trim());
+                }
+
+                if(question.MainQuestion is not null && question.GetLength() == 4)
+                {
+                    ConvertedQuestions.Add(question);
+                }
+            }
+        }
+
+        public void DisplayElement()
+        {
+            foreach (Question question in ConvertedQuestions)
+            {
+                Console.WriteLine(question.ToString());
+            }
+        }
+    }
+}
---
+++ Test_Simplified/source_code/main_program/main_program/Question.cs
@@ -0,0 +1,92 @@
+﻿using DocumentFormat.OpenXml;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main_program
+{
+    class Question: IEnumerable
+    {
+        public string MainQuestion { get; set; }
+        public List<string> Options { get; set; } = new List<string>();
+
+        public Question() { }
+
+        /// <summary>
+        /// Use to add the main question into the object if the object is created by using no-parameter constructor. Should be used along with AddOptions method, or you cannot use any further of its composite methods
+        /// </summary>
+        /// <param name="question"></param>
+        public void AddMainQuestion(string question)
+        {
+            MainQuestion = question;
+        }
+
+        /// <summary>
+        /// Should be use along with AddMainQuestion method, use AddMainQuestion method for how-to-use guide
+        /// </summary>
+        /// <param name="option"></param>
+        public void AddOptions(string option)
+        {
+            if (option.Contains("   "))
+            {
+                string[] strings = option.Split("   ");
+                foreach (string smallString in strings)
+                {
+                    Options.Add(smallString.Trim());
+                }
+            }
+            else
+                Options.Add(option);
+        }
+
+        public void AddOptions(List<string> options)
+        {
+            Options.AddRange(options);
+        }
+
+        public void AddOptions(string[] options)
+        {
+            Options.AddRange(options);
+        }
+
+        public Question(List<string> sentences)
+        {
+            if (!sentences[0].Contains("Câu")) throw new ArgumentException("Cannot find any question or the question is not at the first position of the list");
+            MainQuestion = sentences[0];
+            foreach(var item in sentences)
+            {
+                Options.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// Return the total length of all object's composite elements, including the main question
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">If there is no more than 2 options are passed in</exception>
+        public int GetLength()
+        {
+            if (MainQuestion is null && Options.Count < 2) throw new ArgumentException("Main question and at least 2 options are required.");
+            return Options.Count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(MainQuestion);
+            foreach (var item in Options)
+            {
+                sb.AppendLine(item);
+            }
+            return sb.ToString();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)Options).GetEnumerator();
+        }
+    }
+}
