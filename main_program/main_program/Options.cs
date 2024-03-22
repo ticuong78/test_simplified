c00ecf506efd8d23a9553bab2ee3a1c52387e7ac
ticuong78 <132206463+ticuong78@users.noreply.github.com>
2024-03-22 10:51:55 +0700

    Add files via upload

---
+++ main_program/main_program/Options.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main_program
+{
+    abstract class Options
+    {
+        public List<string> OptionsList { get; set; } = new List<string>();
+        public Options() { }
+
+        public Options(List<string> optionsList) 
+        {
+            OptionsList = optionsList;
+        }
+
+        public int GetLength ()
+        {
+            return OptionsList.Count;
+        }
+
+
+        // Su dung de them option vao OptionList khi su dung constructor khong co param
+        public void AddOptions(string option)
+        {
+            if (option.Contains("   "))
+            {
+                string[] strings = option.Split("   ");
+                foreach (string smallString in strings)
+                {
+                    OptionsList.Add(smallString.Trim());
+                }
+            }
+            else
+                OptionsList.Add(option);
+        }
+
+        public void AddOptions(List<string> options)
+        {
+            OptionsList.AddRange(options);
+        }
+
+        public void AddOptions(string[] options)
+        {
+            OptionsList.AddRange(options);
+        }
+        //----------------------------------------------------------------------
+    }
+}
